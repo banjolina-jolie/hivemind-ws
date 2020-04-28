a37a2c89dc1cd84a8e7e179ebd6747c30fbb4684
Dylan O'Carroll <dylancocarroll@gmail.com>
2020-04-28 15:08:17 -0700

    use ws not uws

--- websocket-server.js
+++ websocket-server.js
@@ -1,6 +1,7 @@
-const redis = require("redis");
+const http = require('http');
+const qs = require('querystring');
+const redis = require('redis');
 const WebSocket = require('ws');
-const uws = require('uWebSockets.js');
 
 const PORT = process.env.PORT || 9001;
 
@@ -12,107 +13,76 @@ if (process.env.REDIS_URL) {
 const isProduction = process.env.NODE_ENV === 'production';
 
 const redisClient = redis.createClient(redisClientOptions);
-const qs = require('querystring');
 
-function buffToString(buf) {
-  return String.fromCharCode.apply(null, new Uint8Array(buf));
-}
+const server = http.createServer();
+const wss = new WebSocket.Server({ noServer: true });
+const questionSubscribers = {};
 
-function stringToBuff(str) {
-  var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
-  var bufView = new Uint8Array(buf);
-  for (var i=0, strLen=str.length; i < strLen; i++) {
-    bufView[i] = str.charCodeAt(i);
+function handleOnConnection(ws, params) {
+  if (params.question) {
+    questionSubscribers[params.question] = questionSubscribers[params.question] || [];
   }
-  return buf;
+
+  if (params.start) {
+    console.log('~~~~~~~~~~~START VOTING~~~~~~~~~~~~~');
+    console.log(params.voting_round_end_time);
+    questionSubscribers[params.question].forEach(ws => {
+      ws.send(JSON.stringify({ start: true, votingRoundEndTime: params.voting_round_end_time}));
+    });
+  } else if (params.vote_next_word) {
+    console.log('~~~~~~~~~~~NEXT VOTING ROUND~~~~~~~~~~~~~');
+    console.log(params.voting_round_end_time);
+    questionSubscribers[params.question].forEach(ws => {
+      ws.send(JSON.stringify({ winningWord: params.winning_word, votingRoundEndTime: params.voting_round_end_time}));
+    });
+  } else {
+    console.log('~~~~~~~~~~~SUBSCRIBE VOTER~~~~~~~~~~~~~');
+    questionSubscribers[params.question].push(ws);
+    const setName = `${params.question}-scores`;
+
+    redisClient.zrevrangebyscore(setName, '+inf', 1, 'withscores', (err, scores) => {
+      if (err) console.log(err);
+      ws.send(JSON.stringify(scores));
+    });
+  }
 }
 
+wss.on('connection', function connection(ws, params) {
 
-// const wss = new WebSocket.Server({ port: PORT });
+  handleOnConnection(ws, params);
 
-// wss.on('connection', function connection(ws) {
-//   console.log('on connection')
-
-//   ws.on('open', (stuff, things) => {
-//     console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-//     console.log('stuff');
-//     console.log(stuff);
-//     console.log('things');
-//     console.log(things);
-//   });
-//   ws.on('message', function incoming(data) {
-//     console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
-//     console.log('data');
-//     console.log(data);
-//     // wss.clients.forEach(function each(client) {
-//     //   if (client.readyState === WebSocket.OPEN) {
-//     //     client.send(data);
-//     //   }
-//     // });
-//   });
-// });
-
-const app = uws./*SSL*/App();
-
-try {
-  app.ws('/*', {
-
-    open: (ws, req) => {
-      console.log('~~~~~~~~~~OPEN~~~~~~~~~~~~')
-      // console.log('ws')
-      // for (var key in ws) {
-      //   console.log(key)
-      // }
-
-      const queryParams = qs.parse(req.getQuery());
-
-      if (queryParams.start) {
-        console.log('~~~~~~~~~~~START VOTING~~~~~~~~~~~~~');
-        if (!ws) {
-          console.log('NO WS COWBOY')
-        } else {
-          ws.publish('start', stringToBuff(JSON.stringify({ start: true, votingRoundEndTime: queryParams.voting_round_end_time})));
-        }
-      } else if (queryParams.vote_next_word) {
-        console.log('~~~~~~~~~~~NEXT VOTING ROUND~~~~~~~~~~~~~');
-        ws.publish('vote-next-word', stringToBuff(JSON.stringify({ winningWord: queryParams.winning_word, votingRoundEndTime: queryParams.voting_round_end_time})));
-      } else {
-        const setName = `${queryParams.question}-scores`;
-
-        redisClient.zrevrangebyscore(setName, '+inf', 1, 'withscores', (err, scores) => {
-          if (err) console.log(err);
-          ws.send(stringToBuff(JSON.stringify(scores)));
-        });
-
-        console.log('~~~~~~~~~~~~~SUBSCRIBING A VOTER~~~~~~~~~~~~~')
-        ws.subscribe('topics/scores');
-        ws.subscribe('start');
-        ws.subscribe('vote-next-word');
-      }
-    },
-
-    message: (ws, message) => {
-      const strMsg = buffToString(message);
-      handleNewVoteMessage(ws, strMsg)
-    }
-
-  }).get('/*', (res, req) => {
-    res.end('Hello World!');
-  }).listen(PORT, (listenSocket, thing) => {
-    console.log('########LISTEN CALLBACK########')
-    if (listenSocket) {
-      console.log('listenSocket');
-      for (var key in listenSocket) {
-        console.log(key);
-      }
-      console.log('Listening...');
-    }
+  ws.on('message', function incoming(data) {
+    handleNewVoteMessage(ws, data)
+    // wss.clients.forEach(function each(client) {
+    //   if (client.readyState === WebSocket.OPEN) {
+    //     client.send(data);
+    //   }
+    // });
   });
-} catch (e) {
-  console.log(`***********ERROR**********`)
-  console.log(e)
-  console.log(e.message)
+});
+
+function authenticate(params, cb) {
+  cb(null);
 }
+
+server.on('upgrade', function upgrade(request, socket, head) {
+  // This function is not defined on purpose. Implement it with your own logic.
+  const qsParams = qs.parse(request.url.slice(2));
+
+  authenticate(qsParams, err => {
+    if (err) {
+      socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n');
+      socket.destroy();
+      return;
+    }
+
+    wss.handleUpgrade(request, socket, head, function done(ws) {
+      wss.emit('connection', ws, qsParams);
+    });
+  });
+});
+
+server.listen(PORT);
 
 function handleNewVoteMessage(ws, strMsg) {
   console.log('strMsg');
@@ -163,7 +133,9 @@ function handleNewVoteMessage(ws, strMsg) {
   function getScoresAndPublish() {
     redisClient.zrevrangebyscore(setName, '+inf', 1, 'withscores', (err, scores) => {
       if (err) console.log(err);
-      ws.publish('topics/scores', stringToBuff(JSON.stringify(scores)));
+      (questionSubscribers[questionId] || []).forEach(ws => {
+        ws.send(JSON.stringify(scores));
+      });
     });
   }
 }
